c9935412847fa177326cc4a4ed3e5ab99c19e9dd
Michael Wangia <michael.w@getslatewell.com>
2023-02-12 12:45:03 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {AppService} from "./app.service";
+import {Message} from "./message";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sockets-ui';
+  response: object | undefined
+  greeting: string = '';
+  name: string = '';
+
+
+  constructor(private appService: AppService) {
+    this.appService.responseSubject.subscribe( data => {
+      this.greeting = data.content
+    });
+  }
+
+  connect() {
+    this.appService.connect()
+  }
+
+  disconnect() {
+    this.appService.disconnect()
+  }
+
+  sendMessage() {
+    this.appService.send(new Message(this.name))
+  }
+
+  // handleMessage() {
+  //   this.greeting = this.response
+  // }
 }
